308d77416f9e19048814a0e7c4a57370b6f484f9
Aditya Singh <cs0640908@gmail.com>
2025-10-02 16:45:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3100-water-bottles-ii/3100-water-bottles-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+    int sum = numBottles;
+    while(numBottles>=numExchange){
+        numBottles = numBottles - numExchange + 1;
+        sum++;
+        numEchange++;
+    }
+   
+   return sum;
+    }
+   
+};
