14848b8c241dc7c7ea031f82f72cd84fbec408f0
Aditya Singh <cs0640908@gmail.com>
2026-06-08 10:22:37 +0530

    Time: 15 ms (11.39%), Space: 127.7 MB (78.53%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -2,34 +2,36 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
        
-       vector<int>small;
-       vector<int>equal;
-       vector<int>greator;
-
-       for( int el : nums){
-          if(el<pivot){
-            small.push_back(el);
-          }
-          else if(el==pivot){
-            equal.push_back(el);
-          }
-          else{
-            greator.push_back(el);
-          }
+      int countleft = 0,pivotcount = 0,greator =0;
+     //finding count of the number
+      for(int i =0;i<nums.size();i++){
+        if(nums[i]<pivot){
+            countleft++;
+        }
+        else if(nums[i]==pivot){
+            pivotcount++;
+        }
+        else{
+            greator++;
+        }
+      }
+     //now i am going to store answer in another vector
+      vector<int>ans(nums.size(),0);
+       int l=0,e=countleft,g=countleft+pivotcount;
+       for(int j=0;j<nums.size();j++){
+         if(nums[j]<pivot){
+            ans[l]=nums[j];
+            l++;
+         }
+         else if(nums[j]==pivot){
+            ans[e]=nums[j];
+            e++;
+         }
+         else{
+            ans[g]=nums[j];
+            g++;
+         }
        }
-
-       vector<int>ans;
-
-       for(int s:small){
-         ans.push_back(s);
-       }
-       for(int e:equal){
-         ans.push_back(e);
-       }
-        for(int g:greator){
-         ans.push_back(g);
-       }
-
-       return ans;
+      return ans;
     }
 };
