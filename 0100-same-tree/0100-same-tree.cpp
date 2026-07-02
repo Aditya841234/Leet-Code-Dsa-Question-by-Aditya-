5c3b196412ddfc8097a7c06c7be65aa86209d394
Aditya Singh <cs0640908@gmail.com>
2026-07-02 11:17:34 +0530

    Time: 0 ms (100%), Space: 12.9 MB (47.61%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,26 +11,42 @@
  */
 class Solution {
 public:
-    void preorder(TreeNode* root, vector<int>& arr){
-        if(root == NULL){
-            arr.push_back(-99);
-            return;
-        }
+    // void preorder(TreeNode* root, vector<int>& arr){
+    //     if(root == NULL){
+    //         arr.push_back(-99);
+    //         return;
+    //     }
 
-        arr.push_back(root->val);
+    //     arr.push_back(root->val);
 
-        preorder(root->left, arr);
-        preorder(root->right, arr);
-    }
+    //     preorder(root->left, arr);
+    //     preorder(root->right, arr);
+    // }
 
     bool isSameTree(TreeNode* p, TreeNode* q) {
 
-        vector<int> p1;
-        vector<int> p2;
+        // vector<int> p1;
+        // vector<int> p2;
 
-        preorder(p, p1);
-        preorder(q, p2);
+        // preorder(p, p1);
+        // preorder(q, p2);
 
-        return p1 == p2;
+        // return p1 == p2;
+
+        if(!p && !q){
+            return 1;
+        }
+
+        if(!p&&q ||p&&!q){
+            return 0;
+        }
+
+       // if the value exist but not to equal of each other 
+        if(p->val != q->val){
+            return 0;
+
+        }
+
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
